851e897462208605cfd017f36f562da20c16c831
gemosant <rasim.mutlu@outlook.com>
2021-02-19 20:02:25 +0300

    find() method explained

---
+++ find_method.js
@@ -0,0 +1,29 @@
+// find() metodu parametre olarak verilen, true/false değer döndüren
+// test fonksiyonunu karşılayan dizi içerisindeki ilk elemanı döndürür.
+
+//bir dizi oluşturalım
+var sehirler = [
+    {
+        isim: "bursa",
+        plaka: 16
+    },
+    {
+        isim: "eskişehir",
+        plaka: 26
+    },
+    {
+        isim: "bilecik",
+        plaka: 11
+    },
+    {
+        isim: "palo alto",
+        plaka: 16
+    },
+]
+
+//bir test fonksiyonu yazalım
+function testFonksiyonu(sehir){
+    return sehir.plaka === 16
+}
+
+console.log(sehirler.find(testFonksiyonu))
