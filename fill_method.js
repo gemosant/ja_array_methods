ed8e05d275ae329391b29408e6a813f4ec350ded
gemosant <rasim.mutlu@outlook.com>
2021-02-19 19:03:18 +0300

    explained fill() method

---
+++ fill_method.js
@@ -0,0 +1,30 @@
+// fill() metodu bir dizideki başlangıçtan (default: 0) 
+// bitişe kadar (default: array.length) elemanları statik değişkene dönüştürür.
+
+/*  
+        kullanımı:
+    array.fill(parametre1, parametre2, parametre3)
+
+    parametre1: diziye yerleştirilecek olan eleman (string, int, bool, double ...)
+    parametre2: değiştirilecek aralığın başlangıç indexi array[i]
+    parametre3: değiştirilecek aralığın bitiş indeksi array[j]
+*/
+
+//örnek olarak bir dizi oluşturalım
+const dizi1 = [true, 3.14, 42, "kodluyoruz.org", 7, "java"]
+console.log("dizi-1: ", dizi1)
+
+dizi1.fill("false", 2, 4) // 2-4 aralığındaki indexleri false ile değiştrdik (2 dahil, 4 dahil değil)
+
+console.log("yeni dizi-1: ", dizi1)
+
+const dizi2 = [["java", "script"], "einstein", "edison", ["react"], "stackoverflow"]
+console.log("eski dizi-2", dizi2)
+
+dizi2.fill("javascript", 0, 1)
+
+console.log("yeni dizi-2", dizi2)
+
+//dizi içindeki diziyi değiştirmek için
+dizi2.fill([true], 3, 4)
+console.log("yepyeni dizi-2", dizi2)
